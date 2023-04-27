331e9934a7c058e5761d75ec5f75a25215ac7c72
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2023-04-27 12:05:15 +0530

    Delete akash-karishma-mentors-freshers-development directory

--- akash-karishma-mentors-freshers-development/app/model/index.js
+++
@@ -1,9 +0,0 @@
-const Auth = require('./auth')
-const Session = require('./session')
-const Otp = require('./otp')
-
-module.exports = {
-  Auth,
-  Session,
-  Otp
-}
--- akash-karishma-mentors-freshers-development/app/routes/v1/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('express')();
-const { verifyApiKey } = require('../../middleware/auth')
-
-app.use('/auth', verifyApiKey, require('./auth'))
-
-
-module.exports = app
-
